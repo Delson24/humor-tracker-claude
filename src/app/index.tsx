e6405bf374d2d163ada9dad77e28c8f3768546fc
Delson <delsonmafumo24@gmail.com>
2026-04-26 18:01:19 +0200

    Corrigido import do componente Titulo

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import Titulo from '@/components/titulo';
+import Titulo from '@/components/Titulo';
 import { useState } from 'react';
 
 export default function App(){
